fb00ac6b00126e1a297f7addbf94aba03677748c
Keashwinraj <you@example.comkeashwinrajrajendran2662@gmail.com>
2023-04-11 14:51:44 +0800

    login

--- quiz3.js
+++ quiz3.js
@@ -7,8 +7,11 @@ app.use(express.json())
 
 app.post('/login', (req, res) => {
     console.log(req.body)
+    
+    let result = login(req.body.username, req.body.password)
 
-    res.send('Login')
+    
+    res.send(result)
   })
 
 app.get('/', (req, res) => {
@@ -20,9 +23,53 @@ app.get('/bye', (req, res) => {
   })
 
   app.post('/register', (req, res) => {
-    res.send('Account Created!')
+    
+    let result = register( req.body.username,req.body.password,req.body.name,req.body.email)
+    
+    res.send(result)
   })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+let dbUsers = [
+    
+  {
+  username: "rAJ",
+  password: "12345",
+  name: "RAJ",
+  email: "b022110211@student.utem.edu.my"
+  },
+  {
+  username: "rAJI",
+  password: "123",
+  name: "RAJI",
+  email: "RAJI@student.utem.edu.my" 
+  },
+  {
+  username: "KEASH",
+  password: "1234",
+  name: "KEASHWINRAJ",
+  email: "KEASHWINRAJ@student.utem.edu.my"
+  }
+]
+function login(reqUsername, reqPassword) {
+  let matchUser =dbUsers.find(user => user.username == reqUsername)
+
+  if(!matchUser) return "User not found!"
+  if(matchUser.password ==reqPassword) {
+      return matchUser
+  } else {
+      return "Invalid password"
+  } 
+}
+
+function register (reqUsername, reqPassword, reqName, reqEmail) {
+  dbUsers.push({
+      username: reqUsername,
+      password: reqPassword,
+      name: reqName,
+      email: reqEmail,
+  })
+
+}
